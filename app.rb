7ada55b17ace8103d37c1654916716833959610b
Andrey Grach <andrejgrach@icloud.com>
2019-06-18 01:28:35 +0300

    Updated Rubygems, added index page and other minor changes

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+
+set :database, "sqlite3:mybloghq.db"
+
+class Post < ActiveRecord::Base
+
+end
+
+class Comment < ActiveRecord::Base
+
+end
+
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+
+  erb :index
 end
