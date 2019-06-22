e9973aeb28d943c1633033a79b81d023cc322122
Andrey Grach <andrejgrach@icloud.com>
2019-06-22 11:40:56 +0300

    Added description

--- app.rb
+++ app.rb
@@ -1,23 +1,26 @@
+#подключаем гемы, так же записал в гемфайл
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
-
+# создём БД
 set :database, "sqlite3:mybloghq.db"
-
+#Считываем модели перед тем как перейти к ним
 before do
   @posts = Post.all
   @comments = Comment.all
   @feedbacks = Feedback.all
 end
-
+#Создаём модель Post -> далее create db:migration -> db:migrate
+# При необходимости делаем Валидацию используем validates
 class Post < ActiveRecord::Base
 has_many :comments 
   validates :personname, presence: true, length: {minimum: 3}
   validates :content, presence: true
 
 end
-
+#Создаём модель Comment  -> далее create db:migration -> db:migrate
+# При необходимости делаем Валидацию используем validates
 class Comment < ActiveRecord::Base
 
   validates :personname, presence: true, length: {minimum: 3}
@@ -25,6 +28,8 @@ class Comment < ActiveRecord::Base
 
 end
 
+#Создаём модель Feedback -> далее create db:migration -> db:migrate
+# При необходимости делаем Валидацию используем validates
 class Feedback < ActiveRecord::Base
 
   validates :username, presence: true, length: {minimum: 3}
@@ -32,22 +37,23 @@ class Feedback < ActiveRecord::Base
 
 end
 
+#Первая страница сайта
 get '/' do
 
   erb :index
 end
-
+#таблица создания поста
 get '/createpost' do
   @p = Post.new params[:createpost]
 
   erb :createpost
 end
-
+#Отправляем инфу на сервер через пост-обработку
 post '/createpost' do
 
   @p = Post.new params[:createpost]
   @p.save
-
+#Проверка валидности данных (через validates в Моделях)
   if @p.save
   erb "<h2>Thank you!</h2>"
   else
@@ -55,18 +61,18 @@ post '/createpost' do
     erb :createpost
     end
 end
-
+#Список созданных постов
 get '/posts' do
 
   @posts = Post.order('created_at DESC')
 
   erb :posts
 end
-
+#На пост пока заглушка он пока не нужен
 post '/posts' do
   erb "Zaglushka"
 end
-
+#По :id подбирается соответсвующий пост какой имеено вывести пост указывает код в представлении postpage.erb
 get '/details/:id' do
   @postinfo = Post.find(params[:id])
   erb :postpage
@@ -74,6 +80,7 @@ get '/details/:id' do
 end
 
 
+#Страница обратной связи. Пока не придумал как правильно сделать отправку мне на почту и плюс не сделал сохраняшку.(в работу)
 get '/feedback' do
 
   @f = Feedback.new
@@ -81,7 +88,7 @@ get '/feedback' do
 
   erb :feedback
 end
-
+#пост обработчик фидбека
 post '/feedback' do
   @f = Feedback.new params[:feedback]
  if @f.save
